693240a90399b1b1e5a579a93074a5cc2254dc62
Nikhil <nikhilpatel64581@gmail.com>
2024-12-05 07:55:07 +0530

    new heap add and remove elements

---
+++ src/PriorityQueue.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class MyPriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(3);
+        pq.add(1);
+        pq.add(2);
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+    }
+}
